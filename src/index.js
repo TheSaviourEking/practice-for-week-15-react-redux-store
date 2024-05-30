e3b676fdbd9da822c857f8dc87fbd60af9b13ad7
TheSaviourEking <saviour.eking@outlook.com>
2024-05-31 00:44:06 +0100

    chore: implement Practice: React-Redux Store Connection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import configureStore from './store';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = configureStore();
+
+const Root = () => {
+  return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
